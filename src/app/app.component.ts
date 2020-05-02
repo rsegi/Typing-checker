b06eb7420d553b703ec6c75f6f40a0a8cfa7b4ad
Raul Segui <r.segui.t@gmail.com>
2020-05-02 19:01:55 +0200

    Fist commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'typing-test';
+  generatedSentence = lorem.sentence();
+  inputText = '';
+  color = '';
+
+  onInput(value: string) {
+    this.inputText = value;
+  }
+
+  compare(generatedLetter: string, inputLetter: string) {
+    if (!inputLetter) {
+      return 'unchecked';
+    }
+    return inputLetter === generatedLetter ? 'correct' : 'wrong';
+  }
 }
